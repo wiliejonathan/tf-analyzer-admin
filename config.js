f7073dd0673262cb5b01e167103b31ec6870aed3
wiliejonathan <117905663+wiliejonathan@users.noreply.github.com>
2026-09-03 20:37:45 +0700

    a

---
+++ config.js
@@ -0,0 +1,5 @@
+window.TF_ADMIN_CONFIG = {
+  // Paste URL Web App Apps Script yang berakhir dengan /exec.
+  // Contoh: https://script.google.com/macros/s/AKfycb.../exec
+  apiUrl: "PASTE_APPS_SCRIPT_EXEC_URL_HERE"
+};
